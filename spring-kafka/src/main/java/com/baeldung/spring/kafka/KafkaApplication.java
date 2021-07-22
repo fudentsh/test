0904a1cbea42e867ea4bd59e952871d23a50849f
fudentsh <fudentsh@gmail.com>
2021-07-22 16:03:06 +0530

    spring kafka'

---
+++ spring-kafka/src/main/java/com/baeldung/spring/kafka/KafkaApplication.java
@@ -0,0 +1,94 @@
+package com.baeldung.spring.kafka;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        ConfigurableApplicationContext context = SpringApplication.run(KafkaApplication.class, args);
+
+        MessageProducer producer = context.getBean(MessageProducer.class);
+        MessageListener listener = context.getBean(MessageListener.class);
+
+        producer.sendMessage("Hello, Earth!");
+        producer.sendMessage("Hello, Mars!");
+        listener.latch.await(10, TimeUnit.SECONDS);
+
+        listener.listenGroupFoo("test");
+
+        context.close();
+    }
+
+    @Bean
+    public MessageProducer messageProducer() {
+        return new MessageProducer();
+    }
+
+    @Bean
+    public MessageListener messageListener() {
+        return new MessageListener();
+    }
+
+    public static class MessageProducer {
+
+        @Autowired
+        private KafkaTemplate<String, String> kafkaTemplate;
+
+        @Autowired
+        private KafkaTemplate<String, Greeting> greetingKafkaTemplate;
+
+        @Value(value = "${message.topic.name}")
+        private String topicName;
+
+        public void sendMessage(String message) {
+
+            ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, message);
+
+            future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+                @Override
+                public void onSuccess(SendResult<String, String> result) {
+                    System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata()
+                        .offset() + "]");
+                }
+
+                @Override
+                public void onFailure(Throwable ex) {
+                    System.out.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+                }
+            });
+        }
+
+    }
+
+    public static class MessageListener {
+
+        private CountDownLatch latch = new CountDownLatch(1);
+
+        @KafkaListener(topics = "${message.topic.name}", groupId = "foo", containerFactory = "fooKafkaListenerContainerFactory")
+        public void listenGroupFoo(String message) {
+            System.out.println("Received Message in group 'foo': " + message);
+            latch.countDown();
+        }
+    }
+
+}
